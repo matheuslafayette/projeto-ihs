ee9e53e173b458cb3130387799888d80d63f4581
matheuslafayette <matheuslafayett@gmail.com>
2023-04-24 15:17:18 -0300

    added main

--- genius-placa.py
+++ genius-placa.py
@@ -1,19 +1,7 @@
 import pygame
 import random
-import serial
 import time
 from utils import *
-
-def file_desc():
-  if len(sys.argv) < 2:
-    print("Error: expected more command line arguments")
-    print("Syntax: %s </dev/device_file>"%sys.argv[0])
-    exit(1)
-
-  fd = os.open(sys.argv[1], os.O_RDWR)
-  return fd
-  
-fd = file_desc()
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -31,10 +19,6 @@ SHADOW_YELLOW = (50, 50, 0)
 # Set the width and height of the screen [width, height]
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 500
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.font.init()
-
-pygame.display.set_caption("Genius Game")
 
 # Define some constants for the game
 FONT_SIZE = 32
@@ -45,8 +29,14 @@ SEQUENCE_DELAY = 1000  # milliseconds
 PRINT_DELAY = 100
 FLASH_DELAY = 500  # milliseconds
 
-# Define the font for displaying text
-font = pygame.font.Font(None, FONT_SIZE)
+def file_desc():
+  if len(sys.argv) < 2:
+    print("Error: expected more command line arguments")
+    print("Syntax: %s </dev/device_file>"%sys.argv[0])
+    exit(1)
+
+  fd = os.open(sys.argv[1], os.O_RDWR)
+  return fd
 
 # Define a class for the colored buttons
 class Button:
@@ -69,144 +59,159 @@ class Button:
         pygame.display.flip()
         pygame.time.wait(delay)
 
-# Set up the colored buttons
-button_colors = [RED, GREEN, BLUE, YELLOW]
-shadow_button_colors = [SHADOW_RED, SHADOW_GREEN, SHADOW_BLUE, SHADOW_YELLOW]
-buttons = []
-for i in range(NUM_BUTTONS):
-    x = SCREEN_WIDTH/2 - (BUTTON_SIZE + BUTTON_MARGIN)*NUM_BUTTONS/2 + (BUTTON_SIZE + BUTTON_MARGIN)*i
-    y = SCREEN_HEIGHT/2 - BUTTON_SIZE/2
-    button = Button(button_colors[i], shadow_button_colors[i], (x, y))
-    buttons.append(button)
+def main():
+  fd = file_desc()
+          
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  pygame.font.init()
 
-# Set up the game loop
-done = False
-clock = pygame.time.Clock()
-level = 1
-sequence = []
+  pygame.display.set_caption("Genius Game")
 
-set_red_leds(fd, 0x40405555)
-set_green_leds(fd, 0x40404055)
+  # Define the font for displaying text
+  font = pygame.font.Font(None, FONT_SIZE)
 
-while not done:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-    
-    # Display the instructions
-    screen.fill(BLACK)
-    if(level == 1):
-        text = font.render("Press any key to start", True, WHITE)
-        screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/2 - text.get_height()/2))
-        pygame.display.flip()
-        # Wait for a key press to start the game
-        waiting = True
-        while waiting:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    waiting = False
-    text = font.render(f"level {level}", True, WHITE)
-    screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/2 + 30 - text.get_height()/2))
-    pygame.display.flip()
-    r_disp(level, fd)
-    pygame.time.wait(1000)
-    
-    # Clear screen
-    screen.fill(BLACK)
-    pygame.display.flip()
-    
-    for button in buttons:
-        button.shadow_draw(screen)
-    
-    pygame.display.flip()
-    pygame.time.wait(1000)
-    
-    # Generate a random sequence of button presses
-    if(level == 1):
-        sequence = []
-    sequence.append(random.randint(0, NUM_BUTTONS-1))
-    
-    # Display the sequence
-    for button_index in sequence:
-        buttons[button_index].flash(screen, FLASH_DELAY)
-        pygame.time.wait(SEQUENCE_DELAY)
-        
-    
-    # Print your turn
-    text = font.render("your turn!", True, WHITE)
-    screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/5 - text.get_height()/2))
-    pygame.display.flip()
-    pygame.time.wait(100)
-    
-    # Get the player's input
-    correct = True
-    player_sequence = []
-    for i in range(len(sequence)):
-        # Clear any pending events before waiting for a key press
-        pygame.event.clear()
-        waiting = True
-        value = 0
-        while waiting and correct:
-            #event = pygame.event.wait()
-            #value = first_b_pres(fd)
-            count = 0
-            check = check_swirch(fd)
-            value = ret_pos()
-            while(count < 3 and ret_pos() == value):
-                print(count)
-                count += 1
-            if(count < 3):
-                value = 0   
-            
-            if check:
-                if value == 1:
-                    player_sequence.append(0)
-                    buttons[0].flash(screen, FLASH_DELAY)
-                    pygame.time.wait(PRINT_DELAY)
-                    if player_sequence[i] != sequence[i]:
-                        correct = False
-                        break
-                    waiting = False
-                elif value == 2:
-                    player_sequence.append(1)
-                    buttons[1].flash(screen, FLASH_DELAY)
-                    pygame.time.wait(PRINT_DELAY)
-                    if player_sequence[i] != sequence[i]:
-                        correct = False
-                        break
-                    waiting = False
-                elif value == 3:
-                    player_sequence.append(2)
-                    buttons[2].flash(screen, FLASH_DELAY)
-                    pygame.time.wait(PRINT_DELAY)
-                    if player_sequence[i] != sequence[i]:
-                        correct = False
-                        break
-                    waiting = False
-                elif value == 4:
-                    player_sequence.append(3)
-                    buttons[3].flash(screen, FLASH_DELAY)
-                    pygame.time.wait(PRINT_DELAY)
-                    if player_sequence[i] != sequence[i]:
-                        correct = False
-                        break
-                    waiting = False
-                
-    # Display the result
-    screen.fill(BLACK)
-    if correct:
-        text = font.render("Congratulations, you win!", True, GREEN)
-        level += 1
-    else:
-        text = font.render("Sorry, you lose!", True, RED)
-        level = 1
-        screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/2 - text.get_height()/2))
-        pygame.display.flip()
-        pygame.time.wait(3000)
-        pygame.quit()
+  # Set up the colored buttons
+  button_colors = [RED, GREEN, BLUE, YELLOW]
+  shadow_button_colors = [SHADOW_RED, SHADOW_GREEN, SHADOW_BLUE, SHADOW_YELLOW]
+  buttons = []
+  for i in range(NUM_BUTTONS):
+      x = SCREEN_WIDTH/2 - (BUTTON_SIZE + BUTTON_MARGIN)*NUM_BUTTONS/2 + (BUTTON_SIZE + BUTTON_MARGIN)*i
+      y = SCREEN_HEIGHT/2 - BUTTON_SIZE/2
+      button = Button(button_colors[i], shadow_button_colors[i], (x, y))
+      buttons.append(button)
 
-    screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/2 - text.get_height()/2))
-    pygame.display.flip()
-    pygame.time.wait(3000)
-    #pygame.quit()
+  # Set up the game loop
+  done = False
+  clock = pygame.time.Clock()
+  level = 1
+  sequence = []
+
+  set_red_leds(fd, 0x40405555)
+  set_green_leds(fd, 0x40404055)
+
+  while not done:
+      # Handle events
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              done = True
+      
+      # Display the instructions
+      screen.fill(BLACK)
+      if(level == 1):
+          text = font.render("Press any key to start", True, WHITE)
+          screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/2 - text.get_height()/2))
+          pygame.display.flip()
+          # Wait for a key press to start the game
+          waiting = True
+          while waiting:
+              for event in pygame.event.get():
+                  if event.type == pygame.KEYDOWN:
+                      waiting = False
+      text = font.render(f"level {level}", True, WHITE)
+      screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/2 + 30 - text.get_height()/2))
+      pygame.display.flip()
+      r_disp(level, fd)
+      pygame.time.wait(1000)
+      
+      # Clear screen
+      screen.fill(BLACK)
+      pygame.display.flip()
+      
+      for button in buttons:
+          button.shadow_draw(screen)
+      
+      pygame.display.flip()
+      pygame.time.wait(1000)
+      
+      # Generate a random sequence of button presses
+      if(level == 1):
+          sequence = []
+      sequence.append(random.randint(0, NUM_BUTTONS-1))
+      
+      # Display the sequence
+      for button_index in sequence:
+          buttons[button_index].flash(screen, FLASH_DELAY)
+          pygame.time.wait(SEQUENCE_DELAY)
+          
+      
+      # Print your turn
+      text = font.render("your turn!", True, WHITE)
+      screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/5 - text.get_height()/2))
+      pygame.display.flip()
+      pygame.time.wait(100)
+      
+      # Get the player's input
+      correct = True
+      player_sequence = []
+      for i in range(len(sequence)):
+          # Clear any pending events before waiting for a key press
+          pygame.event.clear()
+          waiting = True
+          value = 0
+          while waiting and correct:
+              #event = pygame.event.wait()
+              #value = first_b_pres(fd)
+              count = 0
+              check = check_swirch(fd)
+              value = ret_pos()
+              while(count < 3 and ret_pos() == value):
+                  print(count)
+                  count += 1
+              if(count < 3):
+                  value = 0   
+              
+              if check:
+                  if value == 1:
+                      player_sequence.append(0)
+                      buttons[0].flash(screen, FLASH_DELAY)
+                      pygame.time.wait(PRINT_DELAY)
+                      if player_sequence[i] != sequence[i]:
+                          correct = False
+                          break
+                      waiting = False
+                  elif value == 2:
+                      player_sequence.append(1)
+                      buttons[1].flash(screen, FLASH_DELAY)
+                      pygame.time.wait(PRINT_DELAY)
+                      if player_sequence[i] != sequence[i]:
+                          correct = False
+                          break
+                      waiting = False
+                  elif value == 3:
+                      player_sequence.append(2)
+                      buttons[2].flash(screen, FLASH_DELAY)
+                      pygame.time.wait(PRINT_DELAY)
+                      if player_sequence[i] != sequence[i]:
+                          correct = False
+                          break
+                      waiting = False
+                  elif value == 4:
+                      player_sequence.append(3)
+                      buttons[3].flash(screen, FLASH_DELAY)
+                      pygame.time.wait(PRINT_DELAY)
+                      if player_sequence[i] != sequence[i]:
+                          correct = False
+                          break
+                      waiting = False
+                  
+      # Display the result
+      screen.fill(BLACK)
+      if correct:
+          text = font.render("Congratulations, you win!", True, GREEN)
+          level += 1
+      else:
+          text = font.render("Sorry, you lose!", True, RED)
+          level = 1
+          screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/2 - text.get_height()/2))
+          pygame.display.flip()
+          pygame.time.wait(3000)
+          pygame.quit()
+
+      screen.blit(text, (SCREEN_WIDTH/2 - text.get_width()/2, SCREEN_HEIGHT/2 - text.get_height()/2))
+      pygame.display.flip()
+      pygame.time.wait(3000)
+      #pygame.quit()
+      
+
+if __name__ == __main__():
+  main()
